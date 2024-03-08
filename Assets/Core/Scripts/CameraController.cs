e9c93d62c9165e591fbabd7b4517f5cb76efd6cb
flo28rin <florin.viorica28@yahoo.com>
2024-03-08 11:47:40 +0200

    Added main menu visual update

--- Assets/Core/Scripts/CameraController.cs
+++ Assets/Core/Scripts/CameraController.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 public class CameraController : MonoBehaviour
 {
     #region - Declarations
-    [SerializeField]
-    private float duration;
+    [SerializeField] private float location1;
+    [SerializeField] private float location2;
+    [SerializeField] private float duration;
+    [SerializeField] private float speed = 3f;
+
+    Transform crtDest;
     #endregion
 
     #region - Methods
     public void LookAt(Transform target)
     {
         transform.DOLookAt(target.position, duration);
+    }
+
+    public void UpdateCrtDest(Transform t)
+    {
+        crtDest = t;
+    }
+
+    public void Update()
+    {
+        if (crtDest != null)
+        {
+            transform.position = Vector3.Lerp(
+                transform.position,
+                crtDest.position,
+                Time.deltaTime * speed);
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                crtDest.rotation,
+                Time.deltaTime * speed);
+        }
     }
     #endregion
 }
