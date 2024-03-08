01b44b24430d8fa0f0467c6b8095c47dc505a5ae
flo28rin <florin.viorica28@yahoo.com>
2024-03-08 13:21:40 +0200

    added glide on space

---
+++ Assets/Core/Scripts/PlayerGlide.cs
@@ -0,0 +1,33 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGlide : MonoBehaviour
+{
+    ThirdPersonController controller;
+
+    void Awake()
+    {
+        controller = GetComponent<ThirdPersonController>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            controller.Gravity = -2;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            controller.Gravity = -15;
+        }
+    }
+}
