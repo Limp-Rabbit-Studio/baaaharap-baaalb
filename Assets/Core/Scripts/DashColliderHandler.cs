be2ce0757766d9d3de70db7f35976a911a8b1e37
flo28rin <florin.viorica28@yahoo.com>
2024-03-09 20:45:54 +0200

    removed debug logs

--- Assets/Core/Scripts/DashColliderHandler.cs
+++ Assets/Core/Scripts/DashColliderHandler.cs
@@ -19,21 +19,21 @@ public class DashCollisionHandler : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Collided with: " + other.name + "    " + other.tag + "     " + sheepDashAttackScript.IsDashing + "   " + other.GetComponent<EnemyStats>());
+        // Debug.Log("Collided with: " + other.name + "    " + other.tag + "     " + sheepDashAttackScript.IsDashing + "   " + other.GetComponent<EnemyStats>());
         if (other.CompareTag("Enemy") && sheepDashAttackScript.IsDashing)
         {
             EnemyStats enemy = other.GetComponent<EnemyStats>();
             if (enemy != null)
             {
                 enemy.TakeDamage(sheepDashAttackScript.damage);
-                Debug.Log("Dash Collision: Enemy hit with damage.");
+                // Debug.Log("Dash Collision: Enemy hit with damage.");
 
                 // Play bang sound
                 audioSource.PlayOneShot(bangSound);
             }
             else
             {
-                Debug.LogWarning("Dash Collision: The object tagged as 'Enemy' does not have an EnemyStats component.");
+                // Debug.LogWarning("Dash Collision: The object tagged as 'Enemy' does not have an EnemyStats component.");
             }
         }
         // There is no else clause needed here, as we want to ignore all other collisions
